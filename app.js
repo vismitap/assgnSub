5a2786a58370ea8837be4f82e19a106f52773237
vismitap <vismita.prabhu@gmail.com>
2021-09-21 23:38:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+//////////////////////
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
+const app = express();
+
+// Middleware
+// app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+// Mongo URI
+const mongoURI = process.env.MONGO_URI;
+// mongodb+srv://visp:visp123@cluster0.qpsgf.mongodb.net/assgn_sub?retryWrites=true&w=majority
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+ 
+app.get('/', (res,req)=>{
+  req.render('index');
+})
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+      return new Promise((resolve, reject) => {
+          crypto.randomBytes(16, (err, buf) => {
+              if (err) {
+                  return reject(err);
+              }
+              const filename = buf.toString('hex') + path.extname(file.originalname);
+              const fileInfo = {
+                  filename: filename,
+                  bucketName: 'uploads'
+              };
+              resolve(fileInfo);
+          });
+      });
+  }
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  // res.json({ file: req.file });
+  res.redirect('/');
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
